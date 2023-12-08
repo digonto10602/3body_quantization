3ae427360116df8e4093183e50e4fd4ee15b91e9
digonto10602 <digonto10602@gmail.com>
2023-12-08 15:48:25 -0500

    F2_functions added

---
+++ F2_functions.h
@@ -0,0 +1,12 @@
+#ifndef K2FUNCTIONS_H
+#define K2FUNCTIONS_H
+
+#include<bits/stdc++.h>
+#include "gsl/gsl_sf_dawson.h"
+//#include<Eigen/Dense>
+
+/* First we code all the function needed for F2 functions*/
+
+
+
+#endif
