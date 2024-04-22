821fa79d7bb26f4295a91730e70fcbf4dc29b811
Md Habib E Islam <digonto10602@gmail.com>
2024-04-22 09:21:13 -0400

    residues

--- source/printer_F3_omp.cpp
+++ source/printer_F3_omp.cpp
@@ -678,11 +678,283 @@ void test_3body_non_int()
     threebody_non_int_spectrum(filename, m1, m2, m3, total_P, xi, Lbyas, nmax, nsq_max);
 }
 
+void test_F3_pole_datagenerator_for_residue_vs_En_KKpi_omp()
+{
+
+    /*  Inputs  */
+    
+    double L = 20;
+    double Lbyas = L;
+    double xi = 1.0; 
+    double xi1 = 3.444;/* found from lattice */
+    L = L*xi1; // This is done to set the spatial 
+                // unit in terms of a_t everywhere 
+    Lbyas = L; 
+    double scattering_length_1_piK = 4.04;// - 0.2; //total uncertainty 0.05 stat 0.15 systematic 
+    double scattering_length_2_KK = 4.07;// - 0.07; //total uncertainty 0.07 stat 
+    double eta_1 = 1.0;
+    double eta_2 = 0.5; 
+    double atmpi = 0.06906;
+    double atmK = 0.09698;
+
+    //atmpi = atmpi/atmK; 
+    //atmK = 1.0;
+    double energy_cutoff = 0.39; // This is energy cutoff we used in redstar 
+                                 // We use this for getting the residues as well 
+
+    double alpha = 0.5;
+    double epsilon_h = 0.0;
+    int max_shell_num = 20;
+
+    double pi = std::acos(-1.0); 
+    comp twopibyL = 2.0*pi/L;
+    comp twopibyxiLbyas = 2.0*pi/(xi*Lbyas);
+
+    /*---------------------------------------------------*/
+
+    /*---------------------P config----------------------*/
+    int nPmax = 20;
+    std::vector<std::vector<int> > nP_config(3,std::vector<int>());
+
+    for(int i=0;i<nPmax+1;++i)
+    {
+        for(int j=0;j<nPmax+1;++j)
+        {
+            for(int k=0;k<nPmax+1;++k)
+            {
+                int nsq = i*i + j*j + k*k;
+                if(nsq<=4)
+                {
+
+                    if(i>=j && j>=k)
+                    {
+                        std::cout<<"P config:"<<std::endl;
+                        std::cout<<i<<'\t'<<j<<'\t'<<k<<std::endl; 
+
+                        nP_config[0].push_back(i);
+                        nP_config[1].push_back(j);
+                        nP_config[2].push_back(k);
+            
+                    }
+                }
+            }
+        }
+    } 
+
+
+    int P_config_size = nP_config[0].size();
+
+    /*-----------------------------------------------------*/
+
+    for(int ind1=0;ind1<P_config_size;++ind1)
+    {
+        int nPx = nP_config[0][ind1];
+        int nPy = nP_config[1][ind1];
+        int nPz = nP_config[2][ind1];
+    
+        std::string drive = "/home/digonto/Codes/Practical_Lattice_v2/3body_quantization/test_files/F3inv_KKpi_pole_testing/";
+
+        std::string spec_filename =  drive  
+                                    + "Kdf0_spectrum_nP_"
+                                    + std::to_string((int)nPx)
+                                    + std::to_string((int)nPy)
+                                    + std::to_string((int)nPz)
+                                    + "_L20.dat";
+
+        //std::string filename = "temp";
+        comp Px = ((comp)nPx)*twopibyxiLbyas;//twopibyL;
+        comp Py = ((comp)nPy)*twopibyxiLbyas;//twopibyL;
+        comp Pz = ((comp)nPz)*twopibyxiLbyas;//twopibyL;
+        std::vector<comp> total_P(3);
+        total_P[0] = Px; 
+        total_P[1] = Py; 
+        total_P[2] = Pz; 
+        comp total_P_val = std::sqrt(Px*Px + Py*Py + Pz*Pz);
+
+
+        double mi = atmK;
+        double mj = atmK;
+        double mk = atmpi; 
+        //for nP 100 the first run starts 0.4184939100000000245
+        
+        std::vector<double> F3_spectrum;
+        std::ifstream fin; 
+        fin.open(spec_filename.c_str()); 
+        double temp_a, temp_b;
+        while(fin>>temp_a>>temp_b)
+        {
+            if(temp_b<energy_cutoff)
+            {
+                F3_spectrum.push_back(temp_b);
+            }
+        }
+        fin.close(); 
+
+        std::cout<<"Frame momentum P = "<<nPx<<nPy<<nPz<<std::endl; 
+        std::cout<<"--------------------------------"<<std::endl; 
+
+        for(int spec_ind=0; spec_ind<F3_spectrum.size(); ++spec_ind)
+        {
+            double KKpi_threshold = atmK + atmK + atmpi; 
+            double KKpipi_threshold = 2.0*atmK + 2.0*atmpi; 
+            double KKKK_threshold = 5.0*atmK; 
+
+            double pole_position = F3_spectrum[spec_ind]; 
+            std::cout<<"state "<<spec_ind<<" pole location = "<<pole_position<<std::endl; 
+
+            double spec_initial = pole_position - 1.0e-5;
+            double spec_final = pole_position + 1.0e-5; 
+
+            double En_initial = std::sqrt(spec_initial*spec_initial + abs(total_P_val*total_P_val));//.27;//0.4184939100000000245;//0.26302;
+            double En_final = std::sqrt(spec_final*spec_final + abs(total_P_val*total_P_val));
+            double En_points = 6.0;
+
+            double delE = std::abs(En_initial - En_final)/(En_points);
+
+            std::ofstream fout; 
+            std::string filename =  "calcResidue_F3_nP"
+                                    + std::to_string((int)nPx)
+                                    + std::to_string((int)nPy)
+                                    + std::to_string((int)nPz)
+                                    + "_L20_state_"
+                                    + std::to_string((int)spec_ind) 
+                                    + ".dat";
+
+            fout.open(filename.c_str());
+
+            double* norm_vec = NULL; 
+            norm_vec = new double[(int)En_points+1];
+
+            double* En_vec = NULL; 
+            En_vec = new double[(int)En_points+1];
+
+            double* Ecm_vec = NULL;
+	        Ecm_vec = new double[(int)En_points+1];
+
+            comp* result_F3 = NULL;
+	        result_F3 = new comp[(int)En_points+1];
+
+            comp* result_F2 = NULL; 
+            result_F2 = new comp[(int)En_points+1]; 
+
+            comp* result_K2inv = NULL; 
+            result_K2inv = new comp[(int)En_points+1];
+
+            comp* result_G = NULL; 
+            result_G = new comp[(int)En_points+1];
+
+            comp* result_Hinv = NULL; 
+            result_Hinv = new comp[(int)En_points+1];
+
+	        for(int i=0;i<En_points+1;++i)
+            {
+                norm_vec[i] = 0.0; 
+                En_vec[i] = 0.0;
+                Ecm_vec[i] = 0.0;
+                result_F3[i] = 0.0;
+                result_F2[i] = 0.0;
+                result_K2inv[i] = 0.0;
+                result_G[i] = 0.0;
+                result_Hinv[i] = 0.0; 
+            } 
+
+            int loopcounter = 0; 
+            //int i=0; 
+            #pragma omp parallel for 
+            for(int i=0; i<(int)En_points + 1; ++i)
+            {
+                double En = En_initial + ((double)i)*delE; 
+
+                std::vector< std::vector<comp> > p_config(3,std::vector<comp> ());
+                double config_tolerance = 1.0e-5;
+                //config_maker_1(p_config, En, total_P, mi, mj, mk, L, xi, epsilon_h, config_tolerance );
+
+                std::vector< std::vector<comp> > k_config = p_config; 
+
+
+                int size = p_config[0].size();
+                //std::cout<<"size = "<<size<<std::endl;
+                Eigen::VectorXcd state_vec;   
+                Eigen::MatrixXcd F3_mat;//(Eigen::Dynamic,Eigen::Dynamic);
+                Eigen::MatrixXcd F2_mat;
+                Eigen::MatrixXcd K2i_mat; 
+                Eigen::MatrixXcd G_mat; 
+                Eigen::MatrixXcd Hmatinv; 
+
+                test_F3iso_ND_2plus1_mat_with_normalization(  F3_mat, state_vec, F2_mat, K2i_mat, G_mat, Hmatinv, En, p_config, k_config, total_P, eta_1, eta_2, scattering_length_1_piK, scattering_length_2_KK, atmpi, atmK, alpha, epsilon_h, L, xi, max_shell_num); 
+                //std::cout<<"here E="<<i<<std::endl; 
+
+                comp Ecm_calculated = E_to_Ecm(En, total_P);
+
+                comp F3iso = state_vec.transpose()*F3_mat*state_vec; 
+                comp F2iso = state_vec.transpose()*F2_mat*state_vec;
+                comp K2inv_iso = state_vec.transpose()*K2i_mat*state_vec;
+                comp Giso = state_vec.transpose()*G_mat*state_vec;
+                comp Hmatinv_iso = state_vec.transpose()*Hmatinv*state_vec;
+
+                comp norm1 = state_vec.transpose()*state_vec; 
+                double norm2 = real(norm1); 
+                norm_vec[i] = 1.0/std::sqrt(norm2); 
+                En_vec[i] = En; 
+                Ecm_vec[i] = real(Ecm_calculated); 
+                result_F3[i] = F3iso;
+                result_F2[i] = F2iso; 
+                result_G[i] = Giso; 
+                result_K2inv[i] = K2inv_iso; 
+                result_Hinv[i] = Hmatinv_iso; 
+
+            
+            }
+
+            for(int i=0;i<En_points+1;++i)
+            {
+                //std::cout<<std::setprecision(20)<<i<<'\t'<<Ecm_vec[i]<<'\t'<<result_F3[i]<<std::endl; 
+                fout<<std::setprecision(20)
+                    <<En_vec[i]<<'\t'
+                    <<Ecm_vec[i]<<'\t'
+                    <<norm_vec[i]<<'\t'
+                    <<real(result_F3[i])<<'\t'
+                    <<real(1.0/result_F3[i])<<'\t'
+                    <<real(result_F2[i])<<'\t'
+                    <<real(result_G[i])<<'\t'
+                    <<real(result_K2inv[i])<<'\t'
+                    <<real(result_Hinv[i])<<std::endl; 
+            }
+            fout.close();
+
+            //std::cout<<"residue file generation complete"<<std::endl; 
+
+            delete [] norm_vec;
+            norm_vec = NULL; 
+            delete [] En_vec; 
+            En_vec = NULL; 
+            delete [] Ecm_vec;
+            Ecm_vec = NULL;
+            delete [] result_F3; 
+            result_F3 = NULL;
+            delete [] result_F2; 
+            result_F2 = NULL; 
+            delete [] result_K2inv;
+            result_K2inv = NULL; 
+            delete [] result_G; 
+            result_G = NULL; 
+            delete [] result_Hinv; 
+            result_Hinv = NULL; 
+
+        }  
+        std::cout<<"================================"<<std::endl; 
+
+    }             
+}
+
+
+
 int main()
 {
     //test_detF3inv_vs_En_KKpi_omp();
     //test_detF3inv_vs_En_KKpi_6_diff_ma_omp();
-    test_3body_non_int();
+    //test_3body_non_int();
+    test_F3_pole_datagenerator_for_residue_vs_En_KKpi_omp();
     return 0; 
 }
 
--- source/splines_test.cpp
+++ source/splines_test.cpp
@@ -7,14 +7,15 @@ void xj(    std::vector<double> &xj_vec,
             double b,
             int N    )
 {
-    double h = abs(b-a)/N; 
+    double h = std::abs(b-a)/((double)N); 
     for(int i=0; i<N+1; ++i)
     {
-        double val = a + h*i; 
+        double val = a + h*((double)i); 
         xj_vec.push_back(val); 
     }
 
 }
+
 
 void qj(    std::vector<double> &qj_vec,
             std::vector<double> mu_j_vec,
@@ -180,7 +181,7 @@ void Aji(   std::vector<std::vector<double> > &Aji_vec,
 }
 
 void Cji(   std::vector<std::vector<double> > &Cji_vec, 
-            std::vector<std::vector<double> > Aji,
+            std::vector<std::vector<double> > Aji_vec,
             std::vector<double> qj_vec,
             std::vector<double> xj_vec
         )
@@ -197,7 +198,7 @@ void Cji(   std::vector<std::vector<double> > &Cji_vec,
             }
             else 
             {
-                Cji_vec[j][i] = qj_vec[j]*Cji[j+1][i] + Aji[j][i];
+                Cji_vec[j][i] = qj_vec[j]*Cji_vec[j+1][i] + Aji_vec[j][i];
             } 
         }
     }
@@ -206,13 +207,13 @@ void Cji(   std::vector<std::vector<double> > &Cji_vec,
 void Sij(   std::vector<std::vector<double> > &Sij_vec, 
             double x, 
             std::vector<double> xj_vec, 
-            std::vector<std::vector<double> > Cji   )
+            std::vector<std::vector<double> > Cji_vec   )
 {
     int size = xj_vec.size(); 
     
     for(int i=0; i<size; ++i)
     {
-        for(int j=0; j<size; ++j)
+        for(int j=0; j<size-1; ++j)
         {   
             double delta_ij = 0.0; 
             double delta_iplus1_j = 0.0; 
@@ -239,8 +240,8 @@ void Sij(   std::vector<std::vector<double> > &Sij_vec,
             double xminusxj = x - xj_vec[j]; 
             
             double term1 = (delta_iplus1_j - delta_ij)/hjplus1 ;
-            double term2 = (hjplus1/6.0)*(2.0*Cji[j][i] + Cji[j+1][i]) ;
-            double term3 = 0.5*Cji[j][i] + xminusxj*(1.0/(6.0*hjplus1))*(Cji[j+1][i] - Cji[j][i]) ;
+            double term2 = (hjplus1/6.0)*(2.0*Cji_vec[j][i] + Cji_vec[j+1][i]) ;
+            double term3 = 0.5*Cji_vec[j][i] + xminusxj*(1.0/(6.0*hjplus1))*(Cji_vec[j+1][i] - Cji_vec[j][i]) ;
             
             double Sij_val =    delta_ij 
                                 + xminusxj*(term1 - term2 + xminusxj*term3);
@@ -252,10 +253,211 @@ void Sij(   std::vector<std::vector<double> > &Sij_vec,
     }
 }
 
-int main()
+void Sij_builder(   double x,
+                    std::vector<double> xj_vec,
+                    std::vector<std::vector<double> > &Sij_vec  )
+{
+    int size = xj_vec.size(); 
+
+    std::vector<double> pj_vec(size);
+    std::vector<double> qj_vec(size); 
+    std::vector<double> mu_j_vec(size); 
+    std::vector<std::vector<double> > Aji_vec(size,std::vector<double>(size)); 
+    std::vector<std::vector<double> > Bji_vec(size,std::vector<double>(size)); 
+    std::vector<std::vector<double> > Cji_vec(size,std::vector<double>(size)); 
+
+    mu_j(mu_j_vec, xj_vec); 
+
+    qj(qj_vec, mu_j_vec, xj_vec); 
+
+    pj(pj_vec, qj_vec, mu_j_vec, xj_vec); 
+
+    Bji(Bji_vec, xj_vec); 
+
+    Aji(Aji_vec, Bji_vec, mu_j_vec, pj_vec, xj_vec); 
+
+    Cji(Cji_vec, Aji_vec, qj_vec, xj_vec); 
+
+    Sij(Sij_vec, x, xj_vec, Cji_vec); 
+}
+
+
+void print_vec_1D(  std::string cout_var, 
+                    std::vector<double> vec )
+{
+    for(int i=0; i<vec.size(); ++i)
+    {
+        std::cout<<cout_var<<"["<<i<<"]="<<vec[i]<<std::endl; 
+    }
+}
+
+void print_vec_2D(  std::string cout_var, 
+                    std::vector<std::vector<double> > vec )
+{
+    int size1 = vec.size(); 
+    int size2 = vec[0].size(); 
+
+    for(int i=0; i<size1; ++i)
+    {
+        for(int j=0; j<size2; ++j)
+        {
+            std::cout<<cout_var<<"["<<i<<","<<j<<"]="<<vec[i][j]<<std::endl; 
+        }
+    }
+}
+
+double random_test_function_1( double x )
+{
+    //-10*x + 23*x^2  + 32*x^5/Sqrt[x]
+    double A = -10*x;
+    double B = 23*x*x;
+    double C = 32*x*x*x*x*x/std::sqrt(x);
+    double D = std::sqrt(x)*std::pow(x,5); 
+
+    return A + B + C ; 
+}
+
+void test_spline_function_1()
+{
+    double a = 0.000001; 
+    double b = 1.0; 
+    int N = 50; 
+
+    std::vector<double> xj_vec; 
+    xj(xj_vec, a, b, N); 
+
+    std::vector<double> fj_vec; 
+
+    int size = xj_vec.size(); 
+
+    for(int i=0; i<size; ++i)
+    {
+        fj_vec.push_back(random_test_function_1(xj_vec[i]));
+    }
+
+    std::string filename1 = "xj_fj.dat";
+    std::ofstream fout; 
+    fout.open(filename1.c_str()); 
+    for(int i=0; i<size; ++i)
+    {
+        fout<<std::setprecision(20)<<xj_vec[i]<<'\t'<<fj_vec[i]<<std::endl; 
+    }
+    fout.close(); 
+
+    std::string filename2 = "splines_fdata.dat";
+    fout.open(filename2.c_str()); 
+    double N1 = 100000; 
+
+    for(int i=0; i<N1+1; ++i)
+    {
+        double h = std::abs(b-a)/((double)N1); 
+        double xval = a + ((double)i)*h; 
+
+        std::vector<std::vector<double> > Sij_vec1(size, std::vector<double> (size)); 
+        Sij_builder(xval, xj_vec, Sij_vec1);
+
+        int j_val = 0; 
+        for(int j=0; j<xj_vec.size()-1; ++j)
+        {
+            double xj_check1 = xj_vec[j];
+            double xj_check2 = xj_vec[j+1]; 
+
+            if(xval>=xj_check1 && xval<=xj_check2)
+            {
+                j_val = j; 
+            }
+        }
+
+        double f_spline_val = 0.0; 
+
+        for(int k=0; k<xj_vec.size(); ++k)
+        {
+            f_spline_val = f_spline_val + Sij_vec1[k][j_val]*fj_vec[k];
+        }
+
+        fout<<std::setprecision(20)<<xval<<'\t'<<random_test_function_1(xval)<<'\t'<<f_spline_val<<std::endl; 
+        std::cout<<xval<<'\t'<<random_test_function_1(xval)<<'\t'<<f_spline_val<<std::endl; 
+
+    }
+
+    fout.close(); 
+}
+
+void test_spline()
 {
     //std::vector<std::vector<double> > Cji_vec(3,std::vector<double> (3)); 
-    Cji();
+    //Cji();
+
+    double a = 0.0; 
+    double b = 1.0; 
+    int N = 10; 
+
+    std::vector<double> xj_vec; 
+    xj(xj_vec, a, b, N); 
+
+    int size = xj_vec.size(); 
+    double x = 0.51; 
+    std::vector<std::vector<double> > Sij_vec(size, std::vector<double> (size)); 
+    Sij_builder(x, xj_vec, Sij_vec);
+
+    std::cout<<"size = "<<size<<std::endl; 
+    
+    print_vec_1D("xj",xj_vec);
+    print_vec_2D("Sij",Sij_vec);
+
+    std::ofstream fout; 
+    std::string filename = "check_spline.dat"; 
+    fout.open(filename.c_str()); 
+
+    int N1 = 1000;
+
+    int fixed_i = xj_vec.size()/2; 
+
+    for(int i=0; i<N1+1; ++i)
+    {
+        double h = abs(b-a)/((double)N1); 
+        double xval = a + i*h; 
+
+        std::vector<std::vector<double> > Sij_vec1(size, std::vector<double> (size)); 
+        Sij_builder(xval, xj_vec, Sij_vec1);
+
+        int j_val = 0; 
+        for(int j=0; j<xj_vec.size()-1; ++j)
+        {
+            double xj_check1 = xj_vec[j];
+            double xj_check2 = xj_vec[j+1]; 
+
+            if(xval>=xj_check1 && xval<=xj_check2)
+            {
+                j_val = j; 
+            }
+        }
+
+        std::cout<<"xval = "<<xval<<'\t'<<"fixed i = "<<fixed_i<<'\t'
+                 <<"selected j = "<<j_val<<'\t'
+                 <<"Sij = "<<Sij_vec1[fixed_i][j_val]<<std::endl; 
+
+        fout<<xval<<'\t'
+            <<Sij_vec1[fixed_i][j_val]
+            <<std::endl; 
+
+        double summ = 0.0; 
+        for(int k=0;k<size;++k)
+        {
+            summ = summ + Sij_vec1[k][j_val];
+        }    
+        std::cout<<"sum = "<<summ<<std::endl; 
+    }
+
+    fout.close(); 
+
+
+
+}
+
+int main()
+{
+    test_spline_function_1();
 
     return 0; 
 }
